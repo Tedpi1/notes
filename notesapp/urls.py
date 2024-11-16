f27dc0e221105aa6bb65f0cb38f05a0e069f04c5
tedtony <moroted16@gmail.com>
2024-11-16 20:51:55 +0300

    finally

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from notesapp import views
 
+namespace = "notesapp"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("notes/", views.notespage, name="notes"),
 ]
--- notesapp/views.py
+++ notesapp/views.py
@@ -5,4 +5,8 @@ def index(request):
     return render(request, "base.html")
 
 
+def notespage(request):
+    return render(request, "makenotes.html")
+
+
 # Create your views here.
